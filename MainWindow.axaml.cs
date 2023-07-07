666a5a3b28aeef1998689d79a2d8cff52b994c9f
Siydge <53163523+Siydge@users.noreply.github.com>
2023-07-07 23:15:01 +1000

    Added buttons & moved button events to C#

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -5,36 +5,59 @@ using System;
 using System.IO;
 using Newtonsoft.Json;
 using Avalonia.Interactivity;
+using Avalonia.Media;
+using Avalonia;
+using System.Drawing;
+using Avalonia.Shared.PlatformSupport;
 
 namespace Autodraw;
 
 public partial class MainWindow : Window
 {
     private Settings? _settings = null;
+    public Bitmap loadedBitmap = new Bitmap(1,1);
 
     public MainWindow()
     {
         InitializeComponent();
         Config.init();
+        CloseAppButton.Click += quitApp;
+        MinimizeAppButton.Click += minimizeApp;
     }
+
+    // Window Opening Handles
+
+    private void openSettings(object sender, RoutedEventArgs e)
+    {
+        if (_settings == null) { _settings = new Settings(); } else
+        {
+            if (!_settings.IsActive)
+            {
+                Close();
+            }
+        }
+        _settings.Show();
+    }
+
+    // Toolbar Handles
+
+    private void minimizeApp(object? sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
+    }
+    private void quitApp(object? sender, RoutedEventArgs e)
+    {
+        fullClose();
+    }
+
+    // Core Functions
 
     public void fullClose()
     {
         // Other Cleanup
-        if(_settings != null) { _settings.Close(); }
+        if (_settings != null) { _settings.Close(); }
 
         // Main Cleanup
         Close();
-    }
-
-    private void openSettings(object sender, RoutedEventArgs e)
-    {
-        if (_settings == null) { _settings = new Settings(); }
-        _settings.Show();
-    }
-
-    private void quitApp(object sender, RoutedEventArgs e)
-    {
-        fullClose();
     }
 }
--- Utils.cs
+++ Utils.cs
@@ -1,14 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Drawing;
+using Bitmap = Avalonia.Media.Imaging.Bitmap;
 
 namespace Autodraw
 {
+    public static class ImageExtensions
+    {
+        public static Bitmap ConvertToAvaloniaBitmap(this Image bitmap)
+        {
+            if (bitmap == null)
+                return null;
+            System.Drawing.Bitmap bitmapTmp = new System.Drawing.Bitmap(bitmap);
+            var bitmapdata = bitmapTmp.LockBits(new Rectangle(0, 0, bitmapTmp.Width, bitmapTmp.Height), ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
+            Bitmap bitmap1 = new Bitmap(Avalonia.Platform.PixelFormat.Bgra8888, Avalonia.Platform.AlphaFormat.Premul,
+                bitmapdata.Scan0,
+                new Avalonia.PixelSize(bitmapdata.Width, bitmapdata.Height),
+                new Avalonia.Vector(96, 96),
+                bitmapdata.Stride);
+            bitmapTmp.UnlockBits(bitmapdata);
+            bitmapTmp.Dispose();
+            return bitmap1;
+        }
+    }
+
     public class Config
     {
         public static string FolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "AutoDraw");
