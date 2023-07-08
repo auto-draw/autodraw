db962a8183c11706d97926eba4d8d2a8f0a26e0f
Siydge <53163523+Siydge@users.noreply.github.com>
2023-07-08 21:28:21 +1000

    Create ImageProcessing.cs

---
+++ ImageProcessing.cs
@@ -0,0 +1,24 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autodraw
+{
+    public static class ImageProcessing
+    {
+        public class Filters
+        {
+            public bool Threshold = true;
+            public bool Invert = true;
+        }
+
+        public static SKBitmap ApplyFilter(Filters reqFilters)
+        {
+            reqFilters.Threshold = true
+        }
+    }
+}
