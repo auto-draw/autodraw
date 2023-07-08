f7025ccb5f1ac6465937b897d88581ca8c4fc216
Siydge <53163523+Siydge@users.noreply.github.com>
2023-07-08 20:51:51 +1000

    Update MainWindow.axaml.cs

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -9,48 +9,53 @@ using Avalonia.Media;
 using Avalonia;
 using System.ComponentModel;
 using Avalonia.Media.Imaging;
-using System.Drawing;
 
 namespace Autodraw;
 
 public partial class MainWindow : Window
 {
-    private Settings? _settings = null;
-    private SKBitmap _bitmap = new SKBitmap(315,315,true);
+    private Settings? _settings;
+    private SKBitmap _bitmap = new SKBitmap(318, 318, true);
 
     public MainWindow()
     {
         InitializeComponent();
         Config.init();
-        
-        ImagePreview.Source = ImageExtensions.ConvertToAvaloniaBitmap(_bitmap);
 
-        CloseAppButton.Click += quitApp;
-        MinimizeAppButton.Click += minimizeApp;
-        SettingsButton.Click += openSettings;
+        CloseAppButton.Click += QuitApp;
+        MinimizeAppButton.Click += MinimizeApp;
+        SettingsButton.Click += OpenSettings;
+        ProcessButton.Click += ProcessButton_Click;
     }
 
     // External Window Opening/Closing Handles
 
-    private void openSettings(object? sender, RoutedEventArgs e)
+    private void OpenSettings(object? sender, RoutedEventArgs e)
     {
         if (_settings == null) _settings = new Settings();
         _settings.Show();
-        _settings.Closed += closedSettings;
+        _settings.Closed += ClosedSettings;
     }
 
-    private void closedSettings(object? sender, EventArgs e)
+    private void ClosedSettings(object? sender, EventArgs e)
     {
         _settings = null;
     }
 
+
+    private void ProcessButton_Click(object? sender, RoutedEventArgs e)
+    {
+        Bitmap _tmp = ImageExtensions.ConvertToAvaloniaBitmap(_bitmap);
+        ImagePreview.Source = _tmp;
+    }
+
     // Toolbar Handles
 
-    private void minimizeApp(object? sender, RoutedEventArgs e)
+    private void MinimizeApp(object? sender, RoutedEventArgs e)
     {
         WindowState = WindowState.Minimized;
     }
-    private void quitApp(object? sender, RoutedEventArgs e)
+    private void QuitApp(object? sender, RoutedEventArgs e)
     {
         fullClose();
     }
@@ -60,21 +65,9 @@ public partial class MainWindow : Window
     public void fullClose()
     {
         // Other Cleanup
-        if (_settings != null) { _settings.Close(); }
+        _settings?.Close();
 
         // Main Cleanup
         Close();
-    }
-
-    // User Configuration files
-
-    public void loadConfig(string path)
-    {
-
-    }
-
-    public void loadConfigViaDialog()
-    {
-
     }
 }
