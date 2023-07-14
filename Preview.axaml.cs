275b202f076a3a47f7f87a27c7dfb02dd35cbaca
Siydge <53163523+Siydge@users.noreply.github.com>
2023-07-14 21:17:18 +1000

    Preview

---
+++ Preview.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Autodraw;
+
+public partial class Preview : Window
+{
+    public Preview()
+    {
+        InitializeComponent();
+    }
+}
